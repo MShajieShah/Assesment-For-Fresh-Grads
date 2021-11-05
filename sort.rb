3de3239fded3520bd759a041da45206375e2b90b
Abdur Rehman <43990250+Coder1221@users.noreply.github.com>
2021-11-05 11:55:51 +0500

    Update sort.rb

--- sort.rb
+++ sort.rb
@@ -1,9 +1,7 @@
-# Write a sort function to sort the following string. You are not allowed to use any builtin sort function that are provided by language libraries. 
-
+# Write a sort function to sort the following string. You are not allowed to use any builtin sort function that are provided by language libraries.
 # You have to sort the string first according to digits and then according to alphabetical order.
-
-# Write your own sort function that you think that  it will be suitable in case of this type of data.
-
+# Write your own sort function that you think, will be suitable in case of this type of data and will be faster.
+# Also write your reasoning why you are choosing certain algorithum. And write the time and space complexity
 
 # Example
     # Input  "0a12b3c4e5r"
@@ -16,7 +14,3 @@
 # 8923092103912391923120931209031923019230192039120391203912039120312093
 # 012930sadksnakjdnasjkdlasndlkjsandlajsdnlasjdnlkjsnldkjasdjasldkasjdnla
 # jsdasdnasdaksdasndkanl
-
-# So write the sort function which is more suitable for this type of requirement and will be faster.
-
-# Also write your reasoning why you are choosing certain algorithum. And write the time and space complexity
