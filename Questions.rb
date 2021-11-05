4dbc909b4e28e5edec7952d206dc77040c3b0c7b
Coder1221 <21100328@lums.edu.pk>
2021-11-05 12:32:44 +0500

    Updated

--- Questions.rb
+++ Questions.rb
@@ -1,5 +1,11 @@
 # What are highorder functions?
 # Ans:
 
+
 # What is space and time complexity?
+# Ans:
+
+
+# Git concept
+# You should have use git push origin branch To push code in to certain branch. What does origin mean in this command?
 # Ans:
