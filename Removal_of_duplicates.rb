bc3ed1db5a3f978a71fd55a0d8d98b89d7246f95
Coder1221 <21100328@lums.edu.pk>
2021-11-05 12:38:30 +0500

    Only Readme in Main branch

--- Removal_of_duplicates.rb
+++
@@ -1,8 +0,0 @@
-#Write a function to remove duplicates from sorted array. Dont use any ruby builtin functions. Create your own functions
-
-# Example:
-    #Input [111 ,112,112, 114 , 115, 116, 117 , 118 , 119]
-    #Output [111,114,115,116,117,118,119]
-
-    #Input [1,1,1,1,2,2,3,3,4,4,5,6]
-    #Output [1,2,3,4,5,6]
