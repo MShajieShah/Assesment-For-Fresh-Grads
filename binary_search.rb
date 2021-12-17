9e63b12dac903c4831226cbf6ab7370e37a3cac6
MShajieShah <shajieshah@gmail.com>
2021-12-17 16:06:24 +0500

    Added Binary Search Solution

--- binary_search.rb
+++ binary_search.rb
@@ -1,3 +1,22 @@
-# You have to implement binary search. 
-# But the catch here is that numbers will be sorted in reverse order. 
+# You have to implement binary search.
+# But the catch here is that numbers will be sorted in reverse order.
 # Write binary search algorithm for that case.
+def binary_search(arr, n)
+  a = 0
+  b = arr.size
+  loop do
+    mid = (a + b) / 2
+    return n if arr[mid] == n
+    return false if arr[mid] == nil
+    return false if (b - a).abs == 1
+    if arr[mid] < n
+      b = mid
+    else
+      a = mid
+    end
+  end
+end
+
+p binary_search([5, 4, 3, 2, 1], 5)
+p binary_search([5, 4, 3, 2, 1], 1)
+p binary_search([5, 4, 3, 2, 1], -10)
