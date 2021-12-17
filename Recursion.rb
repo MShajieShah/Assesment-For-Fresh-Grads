e2cd64686d3418f2f8155a877a32fc16e4fcf021
MShajieShah <shajieshah@gmail.com>
2021-12-17 13:52:01 +0500

    Added Resursion Solution

--- Recursion.rb
+++ Recursion.rb
@@ -2,7 +2,19 @@
 # Input: [1,[2],[[3]] ,4,[[[[[7]]]]]]
 # Output:  [1,2,3,4,7]
 
-
 # Input: [[[1]],2,[[3]] ,4,[[[[[7]]]]]]
 # Output:  [1,2,3,4,7]
 
+def recursive(array, results = [])
+  array.each do |x|
+    if x.class == Array
+      recursive(x, results)
+    else
+      results << x
+    end
+  end
+  results
+end
+
+p recursive([1, [2], [[3]], 4, [[[[[7]]]]]])
+p recursive([[[1]], 2, [[3]], 4, [[[[[7]]]]]])
