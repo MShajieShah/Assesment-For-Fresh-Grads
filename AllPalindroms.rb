cf6f5da7e6ed532bd8c5d69d0683f58f065c7555
MShajieShah <shajieshah@gmail.com>
2021-12-17 19:41:42 +0500

    Added All Palindrome Solution

--- AllPalindroms.rb
+++ AllPalindroms.rb
@@ -1,18 +1,31 @@
-# Write a function to get all palindroms in string. 
+# Write a function to get all palindroms in string.
 
 # A plaindorm is a string if we reverse that string it would remain same.
 # like level ,madam ,ada ,
 
-
 # Minimum length of palindrom should be greater than equal to 2.
 
-
 # Examples:
-    #Input "level"
-    #Output ["level" ,"eve"]
+#Input "level"
+#Output ["level" ,"eve"]
 
-    # Input "zxz"
-    # Output ["zxz"]
+# Input "zxz"
+# Output ["zxz"]
 
-    # Input "abcreferdeff"
-    # Output ["refer" , "efe" ,"ff"]
+# Input "abcreferdeff"
+# Output ["refer" , "efe" ,"ff"]
+def get_substrings(string)
+  0.upto(string.length - 1).flat_map do |start|
+    2.upto(string.length - start).map do |length|
+      string[start, length]
+    end
+  end.uniq
+end
+
+substrings = get_substrings("abcreferdeff")
+p substrings
+substrings.each do |substring|
+  if substring.length >= 2
+    p substring if substring.reverse == substring
+  end
+end
