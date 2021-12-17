8817608c562ea23d4cbb8c39be6d8a07f03a7cc9
MShajieShah <shajieshah@gmail.com>
2021-12-17 14:38:28 +0500

    Added Max Consecutive Solution

--- max_consec.rb
+++ max_consec.rb
@@ -8,12 +8,27 @@
 # Output: 'a'
 # Reason: a appear 4 times consective in this string
 
-
 # Input : 'abcdaaaafg'
 # Output: 'a'
 # Reason: a appear 4 times consective in this string
 
-
 # Input : 'qzzzzzzzqzzz'
 # Output: 'z'
 # Reason: z appear 7 times consective in this string
+
+def maximum_consective(str)
+  hash = {}
+  max_char = nil
+  max_char_count = 0
+  str.each_char do |x|
+    count = (hash[x] = (hash[x] || 0) + 1)
+    if count > max_char_count
+      max_char = x
+      max_char_count = count
+    end
+  end
+  return max_char
+end
+
+p maximum_consective("qzzzzzzzqzzz")
+p maximum_consective("aabbfccddeefffggh")
